43d793f14db8dcc04a38e4b8a2ebe6dc169c2257
Marie Briem <mbriem@sylt.zbh.uni-hamburg.de>
2015-10-27 14:25:39 +0100

    Aenderung des Namens der Textdatei
    Von protocol.txt zu move_protocol.txt geaendert um es von dem download-Protokoll untescheiden zu koennen.
    Zudem run_chmod.sh in unseren Ordner verschoben.

--- move.rb
+++ move.rb
@@ -7,7 +7,7 @@ require 'fileutils'
 
 begin
   FileUtils.mv(ARGV[0], ARGV[1])
-  f = File.open("protocol.txt", 'a')
+  f = File.open("move_protocol.txt", 'a')
   f.write "#{Time.now} > file '#{ARGV[0]}' moved to '#{ARGV[1]}'\n"
 rescue
   raise "usage: ruby #{$0} filename destination"
---
+++ run_chmod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chgrp -R gi * 2> /dev/null
+chmod -R g+rwX * 2> /dev/null
