de1a55b8597488972ae5f16e122e93fb0bbb48cd
Sarah Weber <sweber@sylt.zbh.uni-hamburg.de>
2015-11-16 16:55:56 +0100

    Rubyskripte hizugefuegt
    Skripte zum Auslesen von Fastadateien

---
+++ Skripte/fasreadcarma.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+path  = ARGV[0].to_s
+output= ARGV[1].to_s
+
+@names=Array.new
+@n=0
+
+Dir.foreach(path) do |fas|
+  next if fas=='.' or fas=='..'
+  datei = File.open(path+"//"+fas, "r")
+  input= datei.readlines
+  datei.close
+    input.each do |i|
+      if i.match (/^(>|\s*$|\s*#)/)
+	i= i.split("SOURCE_1=")
+	i= i[1].split()
+	
+	name="#{i[0]} #{i[1]}"
+	if !@names.include? name
+	  @names[@n]= name
+	  @n= @n+1
+	end
+      end
+    end
+  end
+
+fout = File.open(output, "w")
+fout.puts path
+fout.write("\n")
+fout.puts @names
+fout.close
